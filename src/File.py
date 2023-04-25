e6132cb6f6c8db346b45c95b3e29c2ae4960db07
mantayjon <Jonas.Mantay@Student.HTW-Berlin.de>
2023-04-25 17:15:07 +0200

    add 2 new classes

---
+++ src/File.py
@@ -0,0 +1,5 @@
+class File:
+    def __init__(self, file_name, file_extension, creation_date):
+        self.file_name = file_name
+        self.file_extension = file_extension
+        self.creation_date = creation_date
---
+++ src/PhotoShoot.py
@@ -0,0 +1,7 @@
+class PhotoShoot:
+    def __init__(self, photo_shoot_name, all_files=[], jpeg=[], raw=[], videos=[]):
+        self.photo_shoot_name = photo_shoot_name
+        self.all_files = all_files
+        self.jpeg = jpeg
+        self.raw = raw
+        self.videos = videos
