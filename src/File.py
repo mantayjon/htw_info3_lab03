98b79cbbd6e2e878bdc21c8676fc8d0ff8a14c30
mantayjon <Jonas.Mantay@Student.HTW-Berlin.de>
2023-04-25 17:59:01 +0200

    changes the file and photoshoot classes

--- src/File.py
+++ src/File.py
@@ -1,5 +1,10 @@
 class File:
-    def __init__(self, file_name, file_extension, creation_date):
+
+    def __init__(self, file_name, creation_date):
         self.file_name = file_name
-        self.file_extension = file_extension
+
+        extension = file_name.split(".")
+
+        self.file_extension = extension[1]
         self.creation_date = creation_date
+
--- src/PhotoShoot.py
+++ src/PhotoShoot.py
@@ -1,5 +1,10 @@
 class PhotoShoot:
-    def __init__(self, photo_shoot_name, all_files=[], jpeg=[], raw=[], videos=[]):
+    def __init__(self, photo_shoot_name):
+        all_files = []
+        jpeg = []
+        raw = []
+        videos = []
+
         self.photo_shoot_name = photo_shoot_name
         self.all_files = all_files
         self.jpeg = jpeg
