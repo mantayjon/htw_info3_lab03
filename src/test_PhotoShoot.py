e9b15398a3b75f60d6cbcbc91e201310be56e6dd
mantayjon <Jonas.Mantay@Student.HTW-Berlin.de>
2023-04-29 11:13:44 +0200

    changes testfile

--- src/test_PhotoShoot.py
+++ src/test_PhotoShoot.py
@@ -5,17 +5,18 @@ from PhotoShoot import PhotoShoot
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.file_01 = File("01.jpeg", 2)
-        self.file_02 = File("01.raw", 1)
-        self.file_03 = File("01.MOV", 4)
-        self.file_04 = File("01.pdf", 3)
+        self.file_01 = File("/Users/jonasmantay/Desktop/i-D.jpg")
+        self.file_02 = File("/Users/jonasmantay/Desktop/i-D.jpg")
+        self.file_03 = File("/Users/jonasmantay/Desktop/i-D.jpg")
+        self.file_05 = File("/Users/jonasmantay/Desktop/i-D.jpg")
+        self.file_04 = File("/Users/jonasmantay/Desktop/i-D.jpg")
 
-        self.files = [self.file_01, self.file_02, self.file_03, self.file_04]
+        self.files = [self.file_01, self.file_02, self.file_03, self.file_04, self.file_05]
 
         self.photo_shoot01 = PhotoShoot("shoot01", self.files)
 
     def test_sortList(self):
-        test_jpg = [self.file_01]
+        test_jpg = [self.file_01, self.file_05]
         test_raw = [self.file_02]
         test_mov = [self.file_03]
         test_pdf = [self.file_04]
@@ -27,5 +28,9 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.photo_shoot01.videos, test_mov)
         self.assertEqual(self.photo_shoot01.other_files, test_pdf)
 
-        if __name__ == '__main__':
-            unittest.main()
+    def test_get_date_taken(self):
+        self.assertEqual(self.file_01.check_date_taken(), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
